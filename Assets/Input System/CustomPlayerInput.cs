e1c91b265849b7d481a2498baed0868f257238b1
Kobby6-lang <koseitutu43@gmail.com>
2024-10-18 13:04:41 +0100

    ?

---
+++ Assets/Input System/CustomPlayerInput.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+
+
+public class CustomPlayerInput : MonoBehaviour
+{
+    public Rigidbody body;
+    Vector2 movement;
+
+    void Reset()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+    }
+
+    void OnJump()
+    {
+        body.AddForce(Vector3.up * 10, ForceMode.Impulse);
+    }
+
+    void FixedUpdate()
+    {
+        body.AddForce(new Vector3(movement.x, 0, movement.y) * 2);
+    }
+}
