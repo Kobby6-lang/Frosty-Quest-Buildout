37746f0a05bb2960ed56697f2ab0ecc9b700d6d1
Kobby6-lang <koseitutu43@gmail.com>
2024-10-21 17:02:29 +0100

    Changes

---
+++ Assets/AnimationAndMovementController.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimationAndMovementController : MonoBehaviour
+{
+    // declare reference variables
+    PlayerInput playerInput;
+    CharacterController characterController;
+    Animator animator;
+
+    // variables to store player input values
+    Vector2 currentMovementInput;
+    Vector3 currentMovement;
+    bool isMovementPressed;
+    float rotationFactorPerFrame = 1.0f;
+
+
+    void Awake()
+    {
+        // initially set reference variables
+        playerInput = new PlayerInput();
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
+        playerInput.CharacterControls.Move.started += context => { currentMovement = context.ReadValue<Vector2>(); 
+            currentMovement.x = currentMovementInput.x;
+            currentMovement.z = currentMovementInput.y;
+            isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
+        };
+
+        playerInput.CharacterControls.Move.canceled += context => {
+            currentMovement = context.ReadValue<Vector2>();
+            currentMovement.x = currentMovementInput.x;
+            currentMovement.z = currentMovementInput.y;
+            isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
+        };
+
+        playerInput.CharacterControls.Move.performed += context => {
+        };
+    }
+
+    void HandleRotation() 
+    {
+        Vector3 positionToLookAt;
+
+        positionToLookAt.x = currentMovementInput.x;
+        positionToLookAt.y = 0.0f;
+        positionToLookAt.z = currentMovement.z;
+
+        Quaternion currentRotation = transform.rotation;
+
+        if (isMovementPressed)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(positionToLookAt);
+           transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame);
+        }
+        
+    }
+
+    void OnMoveventInput(InputAction.CallbackContext context) 
+    {
+        currentMovement = context.ReadValue<Vector2>();
+        currentMovement.x = currentMovementInput.x;
+        currentMovement.z = currentMovementInput.y;
+        isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
+    }
+
+    void HandleAnimation() 
+    {
+        bool isWalking = animator.GetBool("isWalking");
+        bool isRunning = animator.GetBool("isRunning");
+
+        if (isMovementPressed && !isWalking) 
+        {
+            animator.SetBool("isWalking", true);
+        }
+
+        else if (isMovementPressed && !isWalking)
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleRotation();
+        HandleAnimation();
+        characterController.Move(currentMovement * Time.deltaTime);
+    }
+
+    void OnEnable()
+    {
+        // enable the character controls action map
+        playerInput.CharacterControls.Enable();
+    }
+
+    void OnDisable()
+    {
+        // disable the character controls action map
+        playerInput.CharacterControls.Disable();
+    }
+}
